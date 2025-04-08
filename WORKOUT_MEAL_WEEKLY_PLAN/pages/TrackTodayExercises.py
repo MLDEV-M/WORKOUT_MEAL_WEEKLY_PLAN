70e2ece227b92991d58190929625ddac0ec81994
MLDEV-M <marilenawork@hotmail.com>
2025-04-08 13:26:16 +0300

    Update TrackTodayExercises.py
    Do not print stretch on final table

--- WORKOUT_MEAL_WEEKLY_PLAN/pages/TrackTodayExercises.py
+++ WORKOUT_MEAL_WEEKLY_PLAN/pages/TrackTodayExercises.py
@@ -224,7 +224,7 @@ def display_track_exercises_page(st):
             plan = plan[~plan['exercise_id'].isin(list(completed_ids))]
 
             st.success("Your remain exercises.")
-            st.session_state.current_plan = plan.copy()
+            st.session_state.current_plan = plan[plan["table_name"]!="stretch"].copy()
             st.write(st.session_state.current_plan)
         except Exception as e:
             st.error("Please try again. Choose exercise completed!")
