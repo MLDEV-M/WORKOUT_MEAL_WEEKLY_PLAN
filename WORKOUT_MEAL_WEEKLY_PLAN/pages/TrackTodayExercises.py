cd13c2f05cb45992c31656ebe2ef41caa2994bfa
MLDEV-M <marilenawork@hotmail.com>
2025-05-14 00:36:20 +0300

    Update TrackTodayExercises.py
    fix exercise_id format as str

--- WORKOUT_MEAL_WEEKLY_PLAN/pages/TrackTodayExercises.py
+++ WORKOUT_MEAL_WEEKLY_PLAN/pages/TrackTodayExercises.py
@@ -93,15 +93,15 @@ def display_track_exercises_page(st):
         tracking_response = supabase.table("exercise_tracking").select("*").eq("user_id", st.session_state.user['user_id']).gte("completed_date", start_str).lte("completed_date", end_str).execute()
         completed_df = pd.DataFrame(tracking_response.data)
 
-        #fix exercise_id format
-        completed_df['exercise_id'] = completed_df['exercise_id'].astype(int)
+        #fix exercise_id format as str
+        completed_df['exercise_id'] = completed_df['exercise_id'].astype(str)
 
         if completed_df.empty:
             return []  # none completed, return all
         else:
             
             # manually remove stretch exercises
-            stretch_list = [1001, 1002, 1003, 1004, 1005, 1006, 1007]  
+            stretch_list = ['1001', '1002', '1003', '1004', '1005', '1006', '1007']  
             
             # Filter out the exercises that are completed on the given weekday
             completed_on_weekday = completed_df[completed_df['completed_week_day'] == weekday]
